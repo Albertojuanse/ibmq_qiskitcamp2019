fc279aaba9bd974dfb1c8c6cae12e021f3fa84e3
y4izus <yaiza.garcia.mm@gmail.com>
2019-05-25 18:51:24 +0200

    Adds script to create single cross out table with custom number of rows

---
+++ IMBQ_QiskitCamp2019/Assets/Scripts/customNumRows.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class customNumRows : MonoBehaviour
+{
+    public int rowsToShow = 3;
+    public List<TMPro.TextMeshProUGUI> rows = new List<TMPro.TextMeshProUGUI>(5);
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(rowsToShow);
+        if(rowsToShow >= 1 && rowsToShow <= 5) {
+            for (int i = 0; i < rows.Count; i++)
+            {
+                rows[i].gameObject.SetActive(i < rowsToShow);
+            }
+        }
+    }
+}
