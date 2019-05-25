3f590fe1b93d5b130e7b42f0b3865f0161a62bfd
y4izus <yaiza.garcia.mm@gmail.com>
2019-05-25 16:28:43 +0200

    Adds single cross out table

---
+++ IMBQ_QiskitCamp2019/Assets/Scripts/singleTableCrossOut.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class singleTableCrossOut : MonoBehaviour
+{
+
+    public List<Image> crossOutImageList = new List<Image>(5);
+    public List<bool> isCrossOutList = new List<bool>(5);
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < isCrossOutList.Count; i++)
+        {
+            if (isCrossOutList[i])
+                crossOutImageList[i].gameObject.SetActive(true);
+            else
+                crossOutImageList[i].gameObject.SetActive(false);
+        }
+    }
+}
