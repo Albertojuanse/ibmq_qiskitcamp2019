304388dc2ee5ba18a3e58f6fe7514c33597ba4fd
Alejo Cifuentes <alejocifusan@gmail.com>
2019-05-25 21:03:38 +0200

    Adds table with togglers to cross out rows

---
+++ IMBQ_QiskitCamp2019/Assets/Scripts/ControlCrossOutState.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlCrossOutState : MonoBehaviour
+{
+    public int index;
+    public singleTableCrossOut table1;
+    public singleTableCrossOut table2;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Toggle myToggle = GetComponent<Toggle>();
+        myToggle.onValueChanged.AddListener(delegate
+        {
+            ToggleValueChanged(myToggle); 
+        });
+    }
+
+
+    void ToggleValueChanged(Toggle myToggle)
+    {
+        table1.isCrossOutList[index] = !table1.isCrossOutList[index];
+        table2.isCrossOutList[index] = !table2.isCrossOutList[index];
+    }
+}
