51bf09e0c0f9ca5a4cdbe423d0b3f206509ff737
Korgan <korgan00@gmail.com>
2019-05-25 17:20:54 +0200

    First Scene working

---
+++ IMBQ_QiskitCamp2019/Assets/Scripts/SelectionPanel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectionPanel : MonoBehaviour
+{
+
+    public Button addBtt;
+    public SwitchState switchState1;
+    public SwitchState switchState2;
+    public TMPro.TextMeshProUGUI QBaseText;
+    public TMPro.TextMeshProUGUI QValueText;
+
+    public List<QBitManager> managers;
+
+    public float delay = 0;
+    private int currentIndex = 0;
+
+    private void Start() {
+        addBtt.onClick.AddListener(() => Invoke("StablishNext", delay));
+    }
+
+    private void StablishNext() {
+        QBitManager qBitManager = managers[currentIndex];
+        qBitManager.QBase = QBaseText.text;
+        qBitManager.QValue = QValueText.text;
+
+        QBaseText.text = "";
+        QValueText.text = "";
+
+        switchState1.Restart();
+        switchState2.Restart();
+        currentIndex++;
+    }
+
+}
