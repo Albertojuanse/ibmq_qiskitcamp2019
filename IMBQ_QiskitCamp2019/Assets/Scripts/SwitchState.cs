44bb221a441672ea8a4c9bccd2f63693496c8ef3
Korgan <korgan00@gmail.com>
2019-05-25 13:04:48 +0200

    Examples done

---
+++ IMBQ_QiskitCamp2019/Assets/Scripts/SwitchState.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchState : MonoBehaviour
+{
+    private TMPro.TextMeshProUGUI tmp;
+    public Button btt;
+
+    private bool stop = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tmp = GetComponent<TMPro.TextMeshProUGUI>();
+        btt.onClick.AddListener(() => stop = true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!stop)
+            tmp.text = Mathf.Round(Random.value) + "";
+
+    }
+}
